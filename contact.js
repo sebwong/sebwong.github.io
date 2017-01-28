afb2b0c0a4c4808f69a211fe622126f6a9639cd8
Sebastian Wong-Sik-Tsang <s.wongsiktsang@gmail.com>
2017-01-28 00:29:55 -0600

    Testing validation

---
+++ contact.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    $('#form').validate({
+        rules: {
+            name: {
+                required: true
+            },
+            email: {
+              required: true,
+              email: true
+            }
+            phone: {
+                required: true,
+                phoneUS: true
+            }
+        },
+        submitHandler: function (form) {
+          alert('valid submission')
+          var $form = $(form);
+          $form.submit();
+        }
+    });
+
+});
