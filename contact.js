b3d5deb464f454c216d53904735ffc9644bee7fe
Sebastian Wong-Sik-Tsang <s.wongsiktsang@gmail.com>
2017-01-28 00:37:45 -0600

    Testing validation 3

--- contact.js
+++ contact.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
             email: {
               required: true,
               email: true
-            }
+            },
             phone: {
                 required: true,
                 phoneUS: true
